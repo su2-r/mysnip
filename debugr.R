ad7e2c71e29a14dbf15d9b8517755f6112e3cdcf
Susan <87833760+su2-r@users.noreply.github.com>
2024-10-25 15:49:25 -0500

    Add files via upload

---
+++ debugr.R
@@ -0,0 +1,7 @@
+echo "# debug" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ksj-code/debug.git
+git push -u origin main
